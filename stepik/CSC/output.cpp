63ed721d176b1cad74f7e2f68a4d1f4ad0bfdcc7
pchchv <ipchchv@gmail.com>
2022-11-09 10:14:16 +0300

    Implement a output some types

---
+++ stepik/CSC/output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int i = 42;
+    double d = 3.14;
+    const char *s = "C-style string";
+    bool b = true;
+
+    std::cout << "This is an integer " << i << "\n";
+    std::cout << "This is a double " << d << "\n";
+    std::cout << "This is a boolean " << b << "\n";
+    std::cout << "This is a \"" << s << "\"\n";
+
+    return 0;
+}
